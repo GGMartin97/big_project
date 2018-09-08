6f0a0f536b57c018ef4e00a7e49840ae67ef4eb4
GGMartin97 <martinofcode@gmail.com>
2018-09-09 07:54:37 +0800

    start

---
+++ save_temp.py
@@ -0,0 +1,27 @@
+from BMP180 import BMP180
+import time
+import sqlite3
+
+
+
+bmp=BMP180()
+
+while True:
+    conn=sqlite3.connect('DATE.db')
+    c=conn.cursor()
+
+    temp=bmp.read_temperature()
+
+    pressure=bmp.read_pressure()
+
+    altitude=bmp.read_altitude()
+
+    timenow=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+    print(timenow)
+    
+    c.execute("INSERT INTO HOMETEMP VALUES (?,?,?,?)",(timenow,temp,pressure,altitude))
+    print("Records created successfully")
+    c.close()
+    conn.commit()
+    conn.close()
+    time.sleep(30)

4ec94f4497f4171ea9bfec16da0f43fe0cafbe50
GGMartin97 <42999995+GGMartin97@users.noreply.github.com>
2018-09-18 16:47:32 +0800

    数据处理

---
+++ data_small.py
@@ -0,0 +1,18 @@
+import os
+
+
+def getCPUtemperature():
+    res = os.popen('vcgencmd measure_temp').readline()
+    return(res.replace("temp=","").replace("'C\n",""))
+
+def getCPUuse():
+    return(str(os.popen("top -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip()))
+
+def getRAMinfo():
+    p = os.popen('free')
+    i = 0
+    while 1:
+        i = i + 1
+        line = p.readline()
+        if i==2:
+            return(line.split()[1:4])
